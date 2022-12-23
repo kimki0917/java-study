9b8c9b0c3230e4864158e134e5b1674ecee06dc2
승학산-24 <-24@DESKTOP-4M8K726>
2022-12-23 19:33:49 +0900

    수정

--- practice04/src/main/java/prob03/unit.java
+++ practice04/src/main/java/prob03/unit.java
@@ -1,14 +1,14 @@
 package prob03;
 
 public class unit {
-	private int x;
-	private int y;
+	protected int x;
+	protected int y;
 
-	void move(int x, int y) {
+	protected void move(int x, int y) {
 		/* 지정된 위치로 이동 */
 	}
 
-	void stop() {
+	protected void stop() {
 		/* 현재 위치에 정지 */
 	}
 
