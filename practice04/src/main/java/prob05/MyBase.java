0f7196bfb80d8d098e9d6daffa62ec3d52fc319b
승학산-24 <-24@DESKTOP-4M8K726>
2022-12-23 19:48:27 +0900

    수정

--- practice04/src/main/java/prob05/MyBase.java
+++ practice04/src/main/java/prob05/MyBase.java
@@ -17,10 +17,6 @@ public class MyBase extends Base {
 	public void day() {
 		System.out.println("낮에는 열심히 일하자!");
 	}
-	@Override
-	public void night() {
-		System.out.println("night");
-	}
 
 	public void afternoon() {
 		System.out.println("오후도 낮과 마찬가지로 일해야합니다.");
